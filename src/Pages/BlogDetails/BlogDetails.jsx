64e6766170e466e2f2ebf1f927868094ba637813
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-05-15 23:42:20 +0600

    added comment section

--- src/Pages/BlogDetails/BlogDetails.jsx
+++ src/Pages/BlogDetails/BlogDetails.jsx
@@ -1,9 +1,20 @@
+import { useContext, useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../Components/AuthProvider/AuthProvider";
 
 const BlogDetails = () => {
   const blog = useLoaderData();
+  const { user } = useContext(AuthContext);
+  const [comment, setComment] = useState('');
 
   const { title, image, short_description, category, long_description } = blog;
+
+  const handleComment = (e) => {
+    e.preventDefault();
+    setComment(e.target.comment.value);
+
+    console.log(comment, user.email);
+  };
   return (
     <div className="container mx-auto px-3 lg:px-12 lg:my-12 my-6">
       <div className="card bg-base-100 rounded-none">
@@ -21,13 +32,36 @@ const BlogDetails = () => {
         </div>
       </div>
       <div className="card bg-base-100 rounded-none lg:px-12 p-3 pt-8 mt-8">
-        <h2 className="card-title text-3xl lg:text-4xl mb-5">Leave a Comment</h2>
-        <textarea
-          placeholder="Type here"
-          className="textarea bg-base-200 rounded-none textarea-bordered textarea-lg w-full"
-        ></textarea>
-        <div className="card-actions justify-end mt-6">
-          <button className="btn bg-[#805aed] text-white">Post Comment</button>
+        <h2 className="card-title text-3xl lg:text-4xl mb-5">
+          Leave a Comment
+        </h2>
+        <form onSubmit={handleComment}>
+          <textarea
+            placeholder="Type here"
+            className="textarea bg-base-200 rounded-none textarea-bordered textarea-lg w-full"
+            name="comment"
+          ></textarea>
+
+          <div className="card-actions justify-end mt-6">
+            <button className="btn bg-[#805aed] text-white">
+              Post Comment
+            </button>
+          </div>
+        </form>
+
+        <h2 className="card-title text-3xl mb-5 mt-8">
+          Comments
+        </h2>
+        <div className="border-t-2 py-8">
+          <div className="avatar">
+            <div className="w-16 rounded-full">
+              <img src={user.photoURL} />
+            </div>
+            <div className="flex flex-col gap-3 ml-4">
+            <h3 className="text-lg font-bold">{user.displayName}</h3>
+            <h4>{comment}</h4>
+            </div>
+          </div>
         </div>
       </div>
     </div>
